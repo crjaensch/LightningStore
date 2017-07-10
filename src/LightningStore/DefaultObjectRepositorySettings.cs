c05dcbf4b54bb0abb5112f0da5eb7d241de48785
Mykhailo Rybnikov <mihasic@gmail.com>
2017-07-10 10:23:48 +0200

    reorganize a bit

--- src/LightningStore/DefaultObjectRepositorySettings.cs
+++ src/LightningStore/DefaultObjectRepositorySettings.cs
@@ -5,49 +5,14 @@ namespace LightningStore
     using System.IO.Compression;
     using System.Text;
 
+    using static Serializer;
+
     public class DefaultObjectRepositorySettings<T> : ObjectRepositorySettings<T, string>
     {
-        private static Jil.Options s_options = new Jil.Options(
-            false, true, false, Jil.DateTimeFormat.MillisecondsSinceUnixEpoch,
-            false, Jil.UnspecifiedDateTimeKindBehavior.IsUTC
-        );
-
         public DefaultObjectRepositorySettings(string path)
-            : base(path, SerializeImpl, DeserializeImpl, Encoding.UTF8.GetBytes, Encoding.UTF8.GetString)
+            : base(path, SerializeGzipJson<T>, DeserializeGzipJson<T>,
+                SerializeString, DeserializeString)
         {
-        }
-
-        private static byte[] SerializeImpl(T obj)
-        {
-            using (var ms = new MemoryStream())
-            {
-                using (var gzip = new DeflateStream(ms, CompressionLevel.Optimal, true))
-                using (var output = new StreamWriter(gzip, Encoding.UTF8))
-                {
-                    Jil.JSON.Serialize(obj, output, s_options);
-                }
-                // if (ms.Position > 4096)
-                //     Console.WriteLine("EXCEEDS page size");
-                return ms.ToArray();
-            }
-        }
-        private static T DeserializeImpl(byte[] data)
-        {
-            if (data.Length == 0) return default(T);
-            try
-            {
-                using (var ms = new MemoryStream(data))
-                using (var gzip = new DeflateStream(ms, CompressionMode.Decompress, true))
-                using (var input = new StreamReader(gzip, Encoding.UTF8))
-                {
-                    return Jil.JSON.Deserialize<T>(input, s_options);
-                }
-            }
-            catch
-            {
-                Console.WriteLine(Encoding.UTF8.GetString(data));
-                throw;
-            }
         }
     }
 }
---
+++ src/LightningStore/Serializer.cs
@@ -0,0 +1,63 @@
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace LightningStore
+{
+    public static class Serializer
+    {
+        private static Jil.Options s_options = new Jil.Options(
+            false, true, false, Jil.DateTimeFormat.MillisecondsSinceUnixEpoch,
+            false, Jil.UnspecifiedDateTimeKindBehavior.IsUTC
+        );
+
+        public static T DeserializeJson<T>(byte[] data)
+        {
+            using (var ms = new MemoryStream(data))
+            using (var input = new StreamReader(ms, Encoding.UTF8))
+            {
+                return Jil.JSON.Deserialize<T>(input, s_options);
+            }
+        }
+
+        public static byte[] SerializeJson<T>(T obj)
+        {
+            using (var ms = new MemoryStream())
+            {
+                using (var output = new StreamWriter(ms, Encoding.UTF8))
+                {
+                    Jil.JSON.Serialize(obj, output, s_options);
+                }
+                return ms.ToArray();
+            }
+        }
+        public static T DeserializeGzipJson<T>(byte[] data)
+        {
+            using (var ms = new MemoryStream(data))
+            using (var gzip = new DeflateStream(ms, CompressionMode.Decompress, true))
+            using (var input = new StreamReader(gzip, Encoding.UTF8))
+            {
+                return Jil.JSON.Deserialize<T>(input, s_options);
+            }
+        }
+
+        public static byte[] SerializeGzipJson<T>(T obj)
+        {
+            using (var ms = new MemoryStream())
+            {
+                using (var gzip = new DeflateStream(ms, CompressionLevel.Optimal, true))
+                using (var output = new StreamWriter(gzip, Encoding.UTF8))
+                {
+                    Jil.JSON.Serialize(obj, output, s_options);
+                }
+                return ms.ToArray();
+            }
+        }
+
+        public static readonly Func<string, byte[]> SerializeString = Encoding.UTF8.GetBytes;
+        public static readonly Func<byte[], string> DeserializeString = Encoding.UTF8.GetString;
+        public static byte[] SerializeGuid(Guid g) => g.ToByteArray();
+        public static Guid DeserializeGuid(byte[] bs) => new Guid(bs);
+    }
+}
